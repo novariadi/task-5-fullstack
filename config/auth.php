fabac47fed10575d341262eb7aae993943035b8d
novariadi <mnovariadi@gmail.com>
2022-10-20 00:02:33 +0700

    Laravel Passport

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'diver' => 'passport',
+            'provider' => 'users',
+        ],
     ],
 
     /*
